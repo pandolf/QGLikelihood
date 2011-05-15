2253a525678cd26d967e0ff9778d2ad63f5d157c
pandolf <>
2011-05-15 16:39:17 +0000

    added computeQGLikelihoodPU function

--- QGLikelihoodCalculator.C
+++ QGLikelihoodCalculator.C
@@ -10,43 +10,35 @@
 
 
 void getBins_int( int nBins_total, Double_t* Lower, Double_t xmin, Double_t xmax, bool plotLog=true);
+void getBins( int nBins_total, Double_t* Lower, Double_t xmin, Double_t xmax, bool plotLog=true);
 
 
 
 
 // constructor:
 
-QGLikelihoodCalculator::QGLikelihoodCalculator( const std::string& fileName, unsigned int nPtBins ) {
+QGLikelihoodCalculator::QGLikelihoodCalculator( const std::string& fileName, unsigned int nPtBins, unsigned int nRhoBins ) {
 
   histoFile_ = TFile::Open(fileName.c_str());
 
   nPtBins_ = nPtBins;
+  nRhoBins_ = nRhoBins;
 
 }
 
 
 
-
-
-float QGLikelihoodCalculator::computeQGLikelihood( float pt, int nCharged, int nNeutral, float ptD ) {
-
-  return this->computeQGLikelihood( pt, nCharged, nNeutral, ptD, -1. );
-
-}
 
 
 
 float QGLikelihoodCalculator::computeQGLikelihood( float pt, int nCharged, int nNeutral, float ptD, float rmsCand ) {
 
-
   float ptMin = 0.;
   float ptMax = 0.;
 
-  const int nBinsPlusOne(nPtBins_+1);
-  //Double_t ptBins[nBinsPlusOne];
-  Double_t* ptBins = new Double_t[nBinsPlusOne];
-  //fitTools::getBins_int( nBinsPlusOne, ptBins, 15., 1000. );
-  getBins_int( nBinsPlusOne, ptBins, 15., 1000. );
+  const int nPtBinsPlusOne(nPtBins_+1);
+  Double_t ptBins[nPtBinsPlusOne];
+  getBins_int( nPtBinsPlusOne, ptBins, 15., 1000. );
 
 
   if( pt>ptBins[nPtBins_] ) {
@@ -62,6 +54,7 @@ float QGLikelihoodCalculator::computeQGLikelihood( float pt, int nCharged, int n
   } //else
   
   if( ptMax==0. ) return -1.;
+
 
 
   char histoName[200];
@@ -101,6 +94,93 @@ float QGLikelihoodCalculator::computeQGLikelihood( float pt, int nCharged, int n
   if(h1_ptD_quark) delete h1_ptD_quark;
   if(h1_rmsCand_gluon) delete h1_rmsCand_gluon;
   if(h1_rmsCand_quark) delete h1_rmsCand_quark;
+
+  return QGLikelihood;
+
+}
+
+
+
+
+float QGLikelihoodCalculator::computeQGLikelihoodPU( float pt, float rhoPF, int nCharged, int nNeutral, float ptD, float rmsCand ) {
+
+
+  // first look for pt bin:
+
+  float ptMin = 0.;
+  float ptMax = 0.;
+
+  const int nPtBinsPlusOne(nPtBins_+1);
+  Double_t ptBins[nPtBinsPlusOne];
+  //fitTools::getBins_int( nPtBinsPlusOne, ptBins, 15., 1000. );
+  getBins_int( nPtBinsPlusOne, ptBins, 15., 1000. );
+
+
+  if( pt>=ptBins[nPtBins_] ) {
+    ptMin = ptBins[nPtBins_-1];
+    ptMax = ptBins[nPtBins_];
+  } else {
+    for( unsigned int iBin=0; iBin<nPtBins_; ++iBin ) {
+      if( pt>=ptBins[iBin] && pt<ptBins[iBin+1] ) {
+        ptMin = ptBins[iBin];
+        ptMax = ptBins[iBin+1];
+      } //if
+    } //for
+  } //else
+  
+
+  if( ptMax==0. ) return -1.;
+
+
+
+
+  //then look for rho bin:
+
+  const int nRhoBinsPlusOne(nRhoBins_+1);
+  Double_t rhoBins[nRhoBinsPlusOne];
+  getBins( nRhoBinsPlusOne, rhoBins, 0., 20., false );
+
+  int rhoBin=-1;
+
+  if( rhoPF>=rhoBins[nRhoBins_] ) {
+    rhoBin = nRhoBins_-1;
+  } else {
+    for( unsigned int iBin=0; iBin<nRhoBins_; ++iBin ) {
+      if( rhoPF>=rhoBins[iBin] && rhoPF<rhoBins[iBin+1] ) {
+        rhoBin = iBin;
+      } //if
+    } //for
+  } //else
+  
+  if( rhoBin==-1 ) return -1.;
+
+
+  char histoName[300];
+  sprintf( histoName, "rhoBins_pt%.0f_%.0f/nCharged_gluon_pt%.0f_%.0f_rho%d", ptMin, ptMax, ptMin, ptMax, rhoBin);
+  TH1F* h1_nCharged_gluon = (TH1F*)histoFile_->Get(histoName);
+  sprintf( histoName, "rhoBins_pt%.0f_%.0f/nCharged_quark_pt%.0f_%.0f_rho%d", ptMin, ptMax, ptMin, ptMax, rhoBin);
+  TH1F* h1_nCharged_quark = (TH1F*)histoFile_->Get(histoName);
+
+  sprintf( histoName, "rhoBins_pt%.0f_%.0f/nNeutral_gluon_pt%.0f_%.0f_rho%d", ptMin, ptMax, ptMin, ptMax, rhoBin);
+  TH1F* h1_nNeutral_gluon = (TH1F*)histoFile_->Get(histoName);
+  sprintf( histoName, "rhoBins_pt%.0f_%.0f/nNeutral_quark_pt%.0f_%.0f_rho%d", ptMin, ptMax, ptMin, ptMax, rhoBin);
+  TH1F* h1_nNeutral_quark = (TH1F*)histoFile_->Get(histoName);
+
+  sprintf( histoName, "rhoBins_pt%.0f_%.0f/ptD_gluon_pt%.0f_%.0f_rho%d", ptMin, ptMax, ptMin, ptMax, rhoBin);
+  TH1F* h1_ptD_gluon = (ptD>=0.) ? (TH1F*)histoFile_->Get(histoName) : 0;
+  sprintf( histoName, "rhoBins_pt%.0f_%.0f/ptD_quark_pt%.0f_%.0f_rho%d", ptMin, ptMax, ptMin, ptMax, rhoBin);
+  TH1F* h1_ptD_quark = (ptD>=0.) ? (TH1F*)histoFile_->Get(histoName) : 0;
+
+  sprintf( histoName, "rhoBins_pt%.0f_%.0f/rmsCand_gluon_pt%.0f_%.0f_rho%d", ptMin, ptMax, ptMin, ptMax, rhoBin);
+  TH1F* h1_rmsCand_gluon = (rmsCand>=0.) ? (TH1F*)histoFile_->Get(histoName) : 0;
+  sprintf( histoName, "rhoBins_pt%.0f_%.0f/rmsCand_quark_pt%.0f_%.0f_rho%d", ptMin, ptMax, ptMin, ptMax, rhoBin);
+  TH1F* h1_rmsCand_quark = (rmsCand>=0.) ? (TH1F*)histoFile_->Get(histoName) : 0;
+
+
+  float gluonP = likelihoodProduct( nCharged, nNeutral, ptD, rmsCand, h1_nCharged_gluon, h1_nNeutral_gluon, h1_ptD_gluon, h1_rmsCand_gluon );
+  float quarkP = likelihoodProduct( nCharged, nNeutral, ptD, rmsCand, h1_nCharged_quark, h1_nNeutral_quark, h1_ptD_quark, h1_rmsCand_quark );
+
+  float QGLikelihood = quarkP / (gluonP + quarkP );
 
   return QGLikelihood;
 
@@ -147,6 +227,24 @@ void getBins_int( int nBins_total, Double_t* Lower, Double_t xmin, Double_t xmax
     } else {
       Lower[i] = TMath::Ceil(Lower[i-1] + dx);
     }
+
+  }
+
+  Lower[nBins] = xmax;
+
+}
+
+
+
+void getBins( int nBins_total, Double_t* Lower, Double_t xmin, Double_t xmax, bool plotLog) {
+
+  int nBins = nBins_total-1;
+  const double dx = (plotLog) ? pow((xmax / xmin), (1. / (double)nBins)) : ((xmax - xmin) / (double)nBins);
+  Lower[0] = xmin;
+  for (int i = 1; i != nBins; ++i) {
+
+    if (plotLog) Lower[i] = Lower[i-1] * dx;
+    else         Lower[i] = Lower[i-1] + dx;
 
   }
 
