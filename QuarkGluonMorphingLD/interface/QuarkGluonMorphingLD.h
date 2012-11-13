f9ffb9d51b830ba78179f786492630352264a185
pandolf <>
2012-11-13 15:42:31 +0000

    doesnt compile yet

--- QuarkGluonMorphingLD/interface/QuarkGluonMorphingLD.h
+++ QuarkGluonMorphingLD/interface/QuarkGluonMorphingLD.h
@@ -1,6 +1,6 @@
 // ------------------------------------------------------------
 //  
-//    QGLikelihoodCalculator - Class
+//    QuarkGluonMorphingLD - Class
 //    for the computation of the QG likelihood.
 //    Needs files provided by having run the
 //    double fit procedure. output.txt?
@@ -18,46 +18,47 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-using namespace std;
 
 
-class QGLikelihoodCalculator {
+class QuarkGluonMorphingLD {
 
  public:
 
-//obsolete
-  QGLikelihoodCalculator( const std::string& fileNameNC , const std::string &fileNameNN,const std::string& fileNamePTD);
-  QGLikelihoodCalculator( const char * configName="data/config.ini");
+  QuarkGluonMorphingLD( const char * configName="data/config.ini");
+  ~QuarkGluonMorphingLD();
 
-   ~QGLikelihoodCalculator();
+  float computeQuarkGluonMorphingLD( float pt, float rhoPF, float *vars );
 
-  float computeQGLikelihoodPU( float pt, float rhoPF, float *vars );
-//obsolete
-  float computeQGLikelihoodPU( float pt, float rhoPF, int nCharged, int nNeutral, float ptD, float rmsCand=-1. );
 
- //inteded for debug purpose  - onyl new
+
+  // functions needed to read config files:
+  char GetChar(FILE *fr);
+  int ReadBinTxt(const char*fileName,int parameter,TH2F*quark,TH2F *gluon);
+  int ReadParTxt( const char *fileName,std::map< std::pair<int,int>, double*> *parq,std::map< std::pair<int,int>, double*> *parg ,int NPar=2 );
+  char* ReadParameterFromFile(const char*fileName,const char * parName);
+
+  //inteded for debug purpose  - onyl new
   int ComputePars(float pt , float rho,const char varName[], char type, double*par);
 
- private:
 
-    FILE *fr_;
-    int ReadBinTxt(const char*fileName,int parameter,TH2F *quark,TH2F *gluon);
-    int ReadParTxt( const char *fileName,std::map< pair<int,int>, double*> *parq,std::map< pair<int,int>, double*> *parg ,int nPar=2 );
-    char GetChar(FILE *fr);
-    TH2F* ptD0_q,*ptD0_g;
-    TH2F* ptD1_q,*ptD1_g;
-    TH2F* ptD2_q,*ptD2_g;
-    std::map< std::pair<int,int>,double* > *parqNC,*parqNN,*pargNC,*pargNN,*parqPTD,*pargPTD;
-	
-//new 
-	int nVars;
-	vector<string> varName;
-	vector<string> varFunc;
-	map< pair< string, char>, map< pair<int,int> ,double* >* >  AllPar;
-   bool isOldStyle;
-double gammadistr_(double* x, double* par);
-double gammadistr2_(double* x, double* par);
-double functionPtD_(double * x ,double*par);
+ private:
+  
+
+  FILE *fr_;
+  TH2F* ptD0_q,*ptD0_g;
+  TH2F* ptD1_q,*ptD1_g;
+  TH2F* ptD2_q,*ptD2_g;
+  std::map< std::pair<int,int>,double* > *parqNC,*parqNN,*pargNC,*pargNN,*parqPTD,*pargPTD;
+
+
+  int nVars;
+  std::vector<std::string> varName;
+  std::vector<std::string> varFunc;
+  std::map< std::pair< std::string, char>, std::map< std::pair<int,int> ,double* >* >  AllPar;
+  bool isOldStyle;
+  double gammadistr_(double* x, double* par);
+  double gammadistr2_(double* x, double* par);
+  double functionPtD_(double * x ,double*par);
 
 };
 
--- QuarkGluonMorphingLD/src/QuarkGluonMorphingLD.cc
+++ QuarkGluonMorphingLD/src/QuarkGluonMorphingLD.cc
@@ -1,4 +1,4 @@
-#include "QGLikelihoodCalculator.h"
+#include "../interface/QuarkGluonMorphingLD.h"
 
 #include <iostream>
 #include <fstream>
@@ -8,8 +8,9 @@
 #include "TMath.h"
 #include "TF1.h"
 #include <map>
-#include "ReadTxt.C"
 #include <stdio.h>
+#include "../interface/PtBins.h"
+
 using namespace std;
 
 //#define DEBUG
@@ -17,29 +18,11 @@ using namespace std;
 
 // constructor:
 
-#include "ReadParameters.C"
-
-QGLikelihoodCalculator::QGLikelihoodCalculator( const std::string& fileNameNC , const std::string &fileNameNN,const std::string& fileNamePTD) {
-	parqNC=new map< pair<int,int>, double* >;
-	pargNC=new map< pair<int,int>, double* >;
-	parqNN=new map< pair<int,int>, double* >;
-	pargNN=new map< pair<int,int>, double* >;
-	parqPTD=new map< pair<int,int>, double* >;
-	pargPTD=new map< pair<int,int>, double* >;
-	
-	ReadParTxt(fileNameNC.c_str(),parqNC,pargNC);
-	ReadParTxt(fileNameNN.c_str(),parqNN,pargNN);
-	ReadParTxt(fileNamePTD.c_str(),parqPTD,pargPTD,3);
-	isOldStyle=true;
-
-}
-
-QGLikelihoodCalculator::QGLikelihoodCalculator( const char * configName)
+QuarkGluonMorphingLD::QuarkGluonMorphingLD( const char * configName)
 {
-Read A;
-	const char * vars=A.ReadParameterFromFile(configName,"QGFIT4VARS");
-	const char * funcs=A.ReadParameterFromFile(configName,"QGFIT4FUNCS");
-	const char * dir=A.ReadParameterFromFile(configName,"QGFIT4TXTDIR");
+	const char * vars=ReadParameterFromFile(configName,"QGFIT4VARS");
+	const char * funcs=ReadParameterFromFile(configName,"QGFIT4FUNCS");
+	const char * dir=ReadParameterFromFile(configName,"QGFIT4TXTDIR");
 	string dirName(dir);
 	
 	char str[1023];int n; 
@@ -82,15 +65,15 @@ Read A;
 }
 
 // ADD map destructor
-QGLikelihoodCalculator::~QGLikelihoodCalculator()
+QuarkGluonMorphingLD::~QuarkGluonMorphingLD()
 {
 }
 
-double QGLikelihoodCalculator::gammadistr_(double* x, double* par)
+double QuarkGluonMorphingLD::gammadistr_(double* x, double* par)
 {
         return TMath::Exp( - x[0] *par[0]/par[1] ) * TMath::Power(x[0],par[0]-1) * TMath::Power(par[1]/par[0],-par[0])/TMath::Gamma(par[0]) ;
 }
-double QGLikelihoodCalculator::gammadistr2_(double* x, double* par)
+double QuarkGluonMorphingLD::gammadistr2_(double* x, double* par)
 {
 	double alpha=par[1] * par[1]/ (par[0]*par[0]); //par 0 = sigma;  par 1= mean
 	double beta=par[1];
@@ -98,140 +81,14 @@ double QGLikelihoodCalculator::gammadistr2_(double* x, double* par)
 }
 
 //half gamma+ offset
-double QGLikelihoodCalculator::functionPtD_(double * x ,double*par)
+double QuarkGluonMorphingLD::functionPtD_(double * x ,double*par)
 {
         if((x[0]-par[0])<0)return 0;
         return TMath::Exp( - (x[0]-par[0]) *par[1]/par[2] ) * TMath::Power((x[0]-par[0]),par[1]-1) * TMath::Power(par[2]/par[1],-par[1])/TMath::Gamma(par[1]) ;
 }
 
-float QGLikelihoodCalculator::computeQGLikelihoodPU( float pt, float rhoPF, int nCharged, int nNeutral, float ptD, float rmsCand ) {
-//plots are already normalized to unity.
-double Q=1;
-double G=1;
 
-
-TF1 *pol3=new TF1("pol3","[0]+[1]*TMath::Log(x)+[2]*TMath::Log(x)*TMath::Log(x)+[3]*TMath::Log(x)*TMath::Log(x)*TMath::Log(x)",20,3500);//LOG! ->PT
-TF1 *pol1=new TF1("pol1","[0]+[1]*x",0,20); //NOT LOG -> RHO
-
-double *par=new double[5];
-double *x=new double[5];
-double a,b;
-x[0]=nCharged;
-//NC Q
-pol3->SetParameters( (*parqNC)[pair<int,int>(0,0)]);//par0 a
-	a=pol3->Eval(pt);
-pol3->SetParameters( (*parqNC)[pair<int,int>(0,1)]);//par0 a
-	b=pol3->Eval(pt);
-pol1->SetParameter(0,b);pol1->SetParameter(1,a);
-	par[0]=pol1->Eval(rhoPF);
-pol3->SetParameters( (*parqNC)[pair<int,int>(1,0)]);//par0 a
-	a=pol3->Eval(pt);
-pol3->SetParameters( (*parqNC)[pair<int,int>(1,1)]);//par0 a
-	b=pol3->Eval(pt);
-pol1->SetParameter(0,b);pol1->SetParameter(1,a);
-	par[1]=pol1->Eval(rhoPF);
-Q*=gammadistr_(x,par);
-//NC G
-pol3->SetParameters( (*pargNC)[pair<int,int>(0,0)]);//par0 a
-	a=pol3->Eval(pt);
-pol3->SetParameters( (*pargNC)[pair<int,int>(0,1)]);//par0 a
-	b=pol3->Eval(pt);
-pol1->SetParameter(0,b);pol1->SetParameter(1,a);
-	par[0]=pol1->Eval(rhoPF);
-pol3->SetParameters( (*pargNC)[pair<int,int>(1,0)]);//par0 a
-	a=pol3->Eval(pt);
-pol3->SetParameters( (*pargNC)[pair<int,int>(1,1)]);//par0 a
-	b=pol3->Eval(pt);
-pol1->SetParameter(0,b);pol1->SetParameter(1,a);
-	par[1]=pol1->Eval(rhoPF);
-G*=gammadistr_(x,par);
-
-//fprintf(stderr,"NC %.2lf -> %.3lf %.3lf <-",Q/(Q+G),pol3->GetParameter(0),(((*pargNC)[pair<int,int>(1,1)])[0]) ); //DEBUG
-x[0]=nNeutral;
-//NN Q
-pol3->SetParameters( (*parqNN)[pair<int,int>(0,0)]);//par0 a
-	a=pol3->Eval(pt);
-pol3->SetParameters( (*parqNN)[pair<int,int>(0,1)]);//par0 a
-	b=pol3->Eval(pt);
-pol1->SetParameter(0,b);pol1->SetParameter(1,a);
-	par[0]=pol1->Eval(rhoPF);
-pol3->SetParameters( (*parqNN)[pair<int,int>(1,0)]);//par0 a
-	a=pol3->Eval(pt);
-pol3->SetParameters( (*parqNN)[pair<int,int>(1,1)]);//par0 a
-	b=pol3->Eval(pt);
-pol1->SetParameter(0,b);pol1->SetParameter(1,a);
-	par[1]=pol1->Eval(rhoPF);
-Q*=gammadistr_(x,par);
-//NN G
-pol3->SetParameters( (*pargNN)[pair<int,int>(0,0)]);//par0 a
-	a=pol3->Eval(pt);
-pol3->SetParameters( (*pargNN)[pair<int,int>(0,1)]);//par0 a
-	b=pol3->Eval(pt);
-pol1->SetParameter(0,b);pol1->SetParameter(1,a);
-	par[0]=pol1->Eval(rhoPF);
-pol3->SetParameters( (*pargNN)[pair<int,int>(1,0)]);//par0 a
-	a=pol3->Eval(pt);
-pol3->SetParameters( (*pargNN)[pair<int,int>(1,1)]);//par0 a
-	b=pol3->Eval(pt);
-pol1->SetParameter(0,b);pol1->SetParameter(1,a);
-	par[1]=pol1->Eval(rhoPF);
-G*=gammadistr_(x,par);
-
-//fprintf(stderr,"NCN %.2lf \n",Q/(Q+G));//DEBUG
-x[0]=ptD;
-//PtD Q
-pol3->SetParameters( (*parqPTD)[pair<int,int>(0,0)]);//par0 a
-	a=pol3->Eval(pt);
-pol3->SetParameters( (*parqPTD)[pair<int,int>(0,1)]);//par0 a
-	b=pol3->Eval(pt);
-pol1->SetParameter(0,b);pol1->SetParameter(1,a);
-	par[0]=pol1->Eval(rhoPF);
-pol3->SetParameters( (*parqPTD)[pair<int,int>(1,0)]);//par0 a
-	a=pol3->Eval(pt);
-pol3->SetParameters( (*parqPTD)[pair<int,int>(1,1)]);//par0 a
-	b=pol3->Eval(pt);
-pol1->SetParameter(0,b);pol1->SetParameter(1,a);
-	par[1]=pol1->Eval(rhoPF);
-pol3->SetParameters( (*parqPTD)[pair<int,int>(2,0)]);//par0 a
-	a=pol3->Eval(pt);
-pol3->SetParameters( (*parqPTD)[pair<int,int>(2,1)]);//par0 a
-	b=pol3->Eval(pt);
-pol1->SetParameter(0,b);pol1->SetParameter(1,a);
-	par[2]=pol1->Eval(rhoPF);
-Q*=functionPtD_(x,par);
-//PtD G
-pol3->SetParameters( (*pargPTD)[pair<int,int>(0,0)]);//par0 a
-	a=pol3->Eval(pt);
-pol3->SetParameters( (*pargPTD)[pair<int,int>(0,1)]);//par0 a
-	b=pol3->Eval(pt);
-pol1->SetParameter(0,b);pol1->SetParameter(1,a);
-	par[0]=pol1->Eval(rhoPF);
-pol3->SetParameters( (*pargPTD)[pair<int,int>(1,0)]);//par0 a
-	a=pol3->Eval(pt);
-pol3->SetParameters( (*pargPTD)[pair<int,int>(1,1)]);//par0 a
-	b=pol3->Eval(pt);
-pol1->SetParameter(0,b);pol1->SetParameter(1,a);
-	par[1]=pol1->Eval(rhoPF);
-pol3->SetParameters( (*pargPTD)[pair<int,int>(2,0)]);//par0 a
-	a=pol3->Eval(pt);
-pol3->SetParameters( (*pargPTD)[pair<int,int>(2,1)]);//par0 a
-	b=pol3->Eval(pt);
-pol1->SetParameter(0,b);pol1->SetParameter(1,a);
-	par[2]=pol1->Eval(rhoPF);
-G*=functionPtD_(x,par);
-
-
-delete[] par;
-delete[] x;
-delete pol3;
-delete pol1;
-if(Q==0)return 0;
-return float(Q/(Q+G));
-}
-
-
-//new 
-float QGLikelihoodCalculator::computeQGLikelihoodPU( float pt, float rhoPF, float*vars ) {
+float QuarkGluonMorphingLD::computeQuarkGluonMorphingLD( float pt, float rhoPF, float*vars ) {
 double Q=1;
 double G=1;
 
@@ -242,7 +99,7 @@ double G=1;
 double *par=new double[5];
 double *x=new double[5];
 double a,b;
-for(int i=0;i<varName.size();++i){
+for(unsigned int i=0;i<varName.size();++i){
 	//printf("DEBUG %s\n",varName[i].c_str());
 	//printf("DEBUG %s\n",varFunc[i].c_str());
 	int R;	
@@ -281,7 +138,7 @@ if(Q==0)return 0;
 return float(Q/(Q+G));
 }
 
-int QGLikelihoodCalculator::ComputePars(float pt , float rhoPF,const char varName[], char type, double*par)
+int QuarkGluonMorphingLD::ComputePars(float pt , float rhoPF,const char varName[], char type, double*par)
 {
 int R=0;
 TF1 *pol3=new TF1("pol3","[0]+[1]*TMath::Log(x)+[2]*TMath::Log(x)*TMath::Log(x)+[3]*TMath::Log(x)*TMath::Log(x)*TMath::Log(x)",20,3500);//LOG! ->PT
@@ -331,4 +188,139 @@ float a,b;
 
 
 
+char QuarkGluonMorphingLD::GetChar(FILE *fr)
+{
+fpos_t pos;//position in the file
+char c;
+//get position of the line to be analized;
+fgetpos(fr,&pos);
+c=fgetc(fr);
+fsetpos(fr,&pos); //moving back to the beginning of the line
+return c;
+}
 
+int QuarkGluonMorphingLD::ReadBinTxt(const char*fileName,int parameter,TH2F*quark,TH2F *gluon)
+	{
+	FILE *fr=fopen(fileName,"r");
+	if(fr==NULL) return -1;
+	//getting binnig
+	double RhoBins[25];int nRhoBins=20;
+	double PtBins[25];int nPtBins=18;
+	getBins_int(18,PtBins,20,1000,true);
+	PtBins[18]=3500;
+	getBins_int(21,RhoBins,0,20,false);
+
+	double PtBinsMean[25];for(int i=0;i<nPtBins;++i){PtBinsMean[i]=(PtBins[i]+PtBins[i+1])/2.;}  
+	double RhoBinsMean[25];for(int i=0;i<nRhoBins;++i){RhoBinsMean[i]=(RhoBins[i]+RhoBins[i+1])/2.;}  
+		//---
+		char c;	
+		//fscanf(fr,"[quark]\n");//move into the file
+		while (( GetChar(fr) == '[') || (GetChar(fr)== '{'))
+			{
+			c='\0';while(c!='\n'){fscanf(fr,"%c",&c);fprintf(stderr,"%c",c);}
+			} //[quark] {bla}
+		
+		while (( GetChar(fr) != '[') && (GetChar(fr)!= EOF) )
+		{
+			//read a formatted line
+			float ptmin, ptmax, rhomin, rhomax,par[10];int nPar;
+			fscanf(fr,"%f %f %f %f %d %*f %*f",&ptmin,&ptmax,&rhomin,&rhomax,&nPar);nPar-=2;
+			fprintf(stderr,"%f %f %f %f %d %d\n",ptmin,ptmax,rhomin,rhomax,nPar,parameter);	
+		
+			for(int i=0;i<nPar;++i)fscanf(fr,"%f",&par[i]);
+			c='\0';while(c!='\n'){fscanf(fr,"%c",&c);} //go to the end of line
+			//check
+			if(nPar<=parameter){perror("I do not have that parameter\n");break;}
+			//filling the histogram
+			quark->SetBinContent(quark->FindBin((ptmin+ptmax)/2.,(rhomin+rhomax)/2.), par[parameter] );
+		}//end of while: loop on the lines
+		
+		//skip lines that begin with [ or  -> useless
+		while (( GetChar(fr) == '[') || (GetChar(fr)== '{'))
+			{
+			c='\0';while(c!='\n'){fscanf(fr,"%c",&c);fprintf(stderr,"%c",c);}
+			} //[gluon] {bla}
+		
+		while (( GetChar(fr) != '[') && (GetChar(fr)!= EOF))
+		{
+			//read a formatted line
+			float ptmin, ptmax, rhomin, rhomax,par[10];int nPar;
+			fscanf(fr,"%f %f %f %f %d %*f %*f",&ptmin,&ptmax,&rhomin,&rhomax,&nPar);nPar-=2;
+			fprintf(stderr,"%f %f %f %f %d %d\n",ptmin,ptmax,rhomin,rhomax,nPar,parameter);	
+		
+			for(int i=0;i<nPar;++i)fscanf(fr,"%f",&par[i]);
+			c='\0';while(c!='\n'){fscanf(fr,"%c",&c);} //go to the end of line
+			//check
+			if(nPar<=parameter){perror("I do not have that parameter\n");break;}
+			//filling the histogram
+			gluon->SetBinContent(gluon->FindBin((ptmin+ptmax)/2.,(rhomin+rhomax)/2.), par[parameter] );
+		}//end of while: loop on the lines
+		fclose(fr);
+}
+
+int QuarkGluonMorphingLD::ReadParTxt( const char *fileName,std::map< pair<int,int>, double*> *parq,std::map< pair<int,int>, double*> *parg ,int NPar )
+	{
+	FILE *fr=fopen(fileName,"r");
+	if(fr==NULL) return -1;
+	for(int P=0;P<NPar;P++)//NOT TRUE FOR PTD
+		{
+		//(*parq)[pair<int,int>(P,0)]=new double[8];
+		//(*parg)[pair<int,int>(P,1)]=new double[8];
+		double *x=new double[5];
+		fscanf(fr,"%*d aq %lf %lf %lf %lf\n",&x[0],&x[1],&x[2],&x[3]);
+			(*parq)[pair<int,int>(P,1)]=x;
+		x=new double[5];
+		fscanf(fr,"%*d ag %lf %lf %lf %lf\n",&x[0],&x[1],&x[2],&x[3]);
+			(*parg)[pair<int,int>(P,1)]=x;
+		x=new double[5];
+		fscanf(fr,"%*d bq %lf %lf %lf %lf\n",&x[0],&x[1],&x[2],&x[3]);
+			(*parq)[pair<int,int>(P,0)]=x;
+		x=new double[5];
+		fscanf(fr,"%*d bg %lf %lf %lf %lf\n",&x[0],&x[1],&x[2],&x[3]);
+			(*parg)[pair<int,int>(P,0)]=x;
+
+		}
+	return 0;
+	}
+
+
+
+char* QuarkGluonMorphingLD::ReadParameterFromFile(const char*fileName,const char * parName)
+{
+FILE *fr=fopen(fileName,"r");
+if(fr==NULL) return NULL;
+char *R=new char[MAX_STR_LENGTH]; //must be a pointer - should survive outside
+char P[MAX_STR_LENGTH];
+char S[MAX_STR_LENGTH];
+
+//leggi una linea su S
+int STATUS=1;
+while(STATUS!=EOF)
+{
+	char c='\0';
+	int i=0;
+	while ( (STATUS=fscanf(fr,"%c",&c))!=EOF ){
+		if(c=='\0') break;
+		if(c=='\n') break;
+		S[i]=c;i++;
+		}
+	S[i]='\0';
+	i=0;
+	while(S[i]!='=' && S[i]!='\0'){P[i]=S[i];i++;}
+	P[i]='\0';int j=0;
+	if(S[i]=='=')i++;
+	while(S[i]!='\0'){R[j]=S[i];i++;j++;}
+	R[j]='\0';
+	bool isPar=true;
+	for(i=0;;i++)
+		{
+		if(P[i]=='\0' && parName[i]=='\0')break;
+		if(P[i]=='\0' || parName[i]=='\0'){isPar=false; break;} // but not together
+		if(parName[i]!=P[i]){isPar=false;break;}
+		}
+	
+	if(isPar){fclose(fr);return R;}
+}
+fclose(fr);
+return NULL;
+}
