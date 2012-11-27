8a3962b9406ef74715994a4110cb8042607fe47f
pandolf <>
2012-11-27 13:43:15 +0000

    removed constexpr, passed to ints

--- interface/Bins.h
+++ interface/Bins.h
@@ -8,11 +8,11 @@ class Bins{
 public:
 const static int nRhoBins=25; //18
 const static int nPtBins=20; //20
-constexpr static double Pt0=20;  //20
-constexpr static double Pt1=1000; //1000
-constexpr static double Rho0=0;  //2
-constexpr static double Rho1=25; //20
-constexpr static double PtLastExtend=1000; //3500
+const static int Pt0=20;  //20
+const static int Pt1=1000; //1000
+const static int Rho0=0;  //2
+const static int Rho1=25; //20
+const static int PtLastExtend=1000; //3500
 
 static int getBins(double  *bins,int nBins,double MinBin=15.0,double MaxBin=1000.,bool log=false);
 static int getBin(int nBins,double  *bins,double value,double*x0=0,double*x1=0);
