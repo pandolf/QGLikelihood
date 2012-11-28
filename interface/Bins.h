be2c731b9bb7dd8934673d410b17638c186f3e23
pandolf <>
2012-11-28 10:46:40 +0000

    now rho up to 40

--- interface/Bins.h
+++ interface/Bins.h
@@ -6,12 +6,12 @@
 
 class Bins{
 public:
-const static int nRhoBins=25; //18
+const static int nRhoBins=40; //18
 const static int nPtBins=20; //20
 const static int Pt0=20;  //20
 const static int Pt1=1000; //1000
 const static int Rho0=0;  //2
-const static int Rho1=25; //20
+const static int Rho1=40; //20
 const static int PtLastExtend=1000; //3500
 
 static int getBins(double  *bins,int nBins,double MinBin=15.0,double MaxBin=1000.,bool log=false);
