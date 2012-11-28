b3f6a57d557eeafd0b78967a18fa7d066921bfa3
pandolf <>
2012-11-28 15:27:50 +0000

    rho up to 40

--- interface/Bins.h
+++ interface/Bins.h
@@ -12,7 +12,7 @@ const static int Pt0=20;  //20
 const static int Pt1=1000; //1000
 const static int Rho0=0;  //2
 const static int Rho1=40; //20
-const static int PtLastExtend=1000; //3500
+const static int PtLastExtend=4000; //3500
 
 static int getBins(double  *bins,int nBins,double MinBin=15.0,double MaxBin=1000.,bool log=false);
 static int getBin(int nBins,double  *bins,double value,double*x0=0,double*x1=0);
